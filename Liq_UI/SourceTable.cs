86b55709ad8c2ce46f9147ff0d187683eb3fc84e
Boris Town <tbziy@hotmail.com>
2016-03-04 22:32:14 +0800

    SourceTableLine

--- Liq_UI/SourceTable.cs
+++ Liq_UI/SourceTable.cs
@@ -1,7 +1,13 @@
-﻿namespace Liq_UI
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace Liq_UI
 {
     internal class SourceTable
     {
-
+        //Table Fields
+        List<string> TableFields = new List<string>();
+        //Table Contents 
+        List<SourceTableLine> TableContents = new List<SourceTableLine>();
     }
 }
