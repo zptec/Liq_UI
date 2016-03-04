31b5562f64bcc466b85aa71bc9184df00ae770b5
Boris Town <tbziy@hotmail.com>
2016-03-04 19:47:10 +0800

    Commit Test

--- Liq_UI/MainWindow.xaml.cs
+++ Liq_UI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,26 @@ namespace Liq_UI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = GetExcelToDataTableBySheet(this.textBox.Text, "KNA1");
+
+        }
+
+        public static DataTable GetExcelToDataTableBySheet(string FileFullPath, string SheetName)
+        {
+            //string strConn = "Provider=Microsoft.Jet.OleDb.4.0;" + "data source=" + FileFullPath + ";Extended Properties='Excel 8.0; HDR=NO; IMEX=1'"; //此連接只能操作Excel2007之前(.xls)文件
+            string strConn = "Provider=Microsoft.Ace.OleDb.15.0;" + "data source=" + FileFullPath + ";Extended Properties='Excel 15.0; HDR=NO; IMEX=1'"; //此連接可以操作.xls與.xlsx文件
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            DataSet ds = new DataSet();
+            OleDbDataAdapter odda = new OleDbDataAdapter(string.Format("SELECT * FROM [{0}]", SheetName), conn); //("select * from [Sheet1$]", conn);
+            odda.Fill(ds, SheetName);
+            conn.Close();
+
+            return ds.Tables[0];
         }
     }
 }
