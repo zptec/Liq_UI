6e6d8a4dc24830420cd8d3b59ea6bb4a9169caa0
Boris Town <tbziy@hotmail.com>
2016-03-05 21:08:26 +0800

    TargetRelateFilter

--- Liq_UI/TargetField.cs
+++ Liq_UI/TargetField.cs
@@ -1,7 +1,14 @@
-﻿namespace Liq_UI
+﻿using System.Collections.Generic;
+
+namespace Liq_UI
 {
     internal class TargetField
     {
-
+        //Field Value
+        string FieldValue = "";
+        //Relate Cells
+        List<TargetRelateCell> RelateCells = new List<TargetRelateCell>();
+        //Relate Filters
+        List<TargetRelateFilter> RelateFilter = new List<TargetRelateFilter>();
     }
 }
