8ce122f855aa6f69f1bbe3b85c950f4c68ee3e4c
Boris Town <tbziy@hotmail.com>
2016-03-07 13:50:55 +0800

    TrainningCase

--- Liq_UI/Trainning/TrainningBase.cs
+++ Liq_UI/Trainning/TrainningBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Liq_UI.Analysis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Liq_UI/Trainning/TrainningCase.cs
+++ Liq_UI/Trainning/TrainningCase.cs
@@ -1,6 +1,26 @@
-﻿namespace Liq_UI.Trainning
+﻿using Liq_UI.Analysis;
+using Liq_UI.Filter;
+using Liq_UI.Source;
+using Liq_UI.Target;
+using System.Collections.Generic;
+
+namespace Liq_UI.Trainning
 {
     internal class TrainningCase
     {
+        //Upper Object
+        TrainningBase UpperObject = new TrainningBase();
+
+        //Source Data
+        SourceData SourceData = new SourceData();
+
+        //Target Data
+        TargetData TargetDate = new TargetData();
+
+        //Filter Data
+        FilterData FilterData = new FilterData();
+
+        //Analysis Case
+        List<AnalysisCase> AnalysisCase = new List<AnalysisCase>();
     }
 }
