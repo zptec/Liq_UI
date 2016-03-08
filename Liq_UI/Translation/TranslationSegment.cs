4dc5f432d661e0f5262695c8723810f5f46d98f0
Boris Town <tbziy@hotmail.com>
2016-03-08 10:22:05 +0800

    End Line

--- Liq_UI/Translation/TranslationSegment.cs
+++ Liq_UI/Translation/TranslationSegment.cs
@@ -2,6 +2,16 @@
 {
     internal class TranslationSegment
     {
-        //
+        //Begin Line
+        int BeginLine;
+
+        //Line Count
+        int LineCount;
+
+        //End Line
+        int EndLine
+        {
+            get { return BeginLine + LineCount + 1; }
+        }
     }
 }
