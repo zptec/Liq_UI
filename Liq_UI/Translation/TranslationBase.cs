96936931b9214bea49cbfe0abae5659558540cc1
Boris Town <tbziy@hotmail.com>
2016-03-08 10:05:37 +0800

    Translator for ABAP internal table processing

--- Liq_UI/Translation/TranslationBase.cs
+++ Liq_UI/Translation/TranslationBase.cs
@@ -20,14 +20,22 @@ namespace Liq_UI.Translation
         {
             //Translator for ABAP data definition
             TranslationDefinition DefTranslator = new TranslationDefinition(this);
+
             //Translator for ABAP selection screen
             TranslationSelection SelectionTranslator = new TranslationSelection(this);
+
             //Translator for ABAP DB fetching
             TranslationDBFetching DBFetchingTranslator = new TranslationDBFetching(this);
+
             //Translator for ABAP table reading
             TranslationALV ReadingTranslator = new TranslationALV(this);
+
+            //Translator for ABAP internal table processing
+            TranslationProcessing ProcessTranslator = new TranslationProcessing(this);
+
             //Translator for ABAP ALV
             TranslationALV ALVTranslator = new TranslationALV(this);
+
             return 0;
         }
     }
---
+++ Liq_UI/Translation/TranslationProcessing.cs
@@ -0,0 +1,12 @@
+﻿namespace Liq_UI.Translation
+{
+    internal class TranslationProcessing
+    {
+        private TranslationBase translationBase;
+
+        public TranslationProcessing(TranslationBase translationBase)
+        {
+            this.translationBase = translationBase;
+        }
+    }
+}
