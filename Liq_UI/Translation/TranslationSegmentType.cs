fa1102f7ab693da2ac16d7fcd158397a7b754090
Boris Town <tbziy@hotmail.com>
2016-03-08 11:01:26 +0800

    enum TranslationSegmentType

--- Liq_UI/Translation/TranslationSegmentType.cs
+++ Liq_UI/Translation/TranslationSegmentType.cs
@@ -2,6 +2,25 @@
 {
     internal enum TranslationSegmentType
     {
+        //Heading Region
+        Heading = 1,
 
+        //Definition
+        Definition = 2,
+
+        //Selection
+        Selection = 3,
+
+        //DB Fetching
+        DBFetching = 4,
+
+        //Table Reading
+        TableReading = 5,
+
+        //Processing
+        Processing = 6,
+
+        //ALV Output
+        ALV = 7
     }
 }
