5a5290d9f524fa5012b8bb12e734a063208d3d2f
Boris Town <tbziy@hotmail.com>
2016-03-20 17:00:19 +0800

    class AnalysisFormCall

---
+++ Liq_UI/Analysis/AnalysisFormCall.cs
@@ -0,0 +1,20 @@
+﻿namespace Liq_UI.Analysis
+{
+    internal class AnalysisFormCall
+    {
+        /// <summary>
+        /// From deacription
+        /// </summary>
+        public string FormDesc { get; internal set; }
+        
+        /// <summary>
+        /// Form Name
+        /// </summary>
+        public string FormName { get; internal set; }
+
+        /// <summary>
+        /// Form Index
+        /// </summary>
+        public string Index { get; internal set; }
+    }
+}
