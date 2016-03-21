288ebb556582a52060a153686232860279e4079f
Boris Town <tbziy@hotmail.com>
2016-03-22 00:17:08 +0800

    Append ALV CALL Segment

--- Liq_UI/Translation/TranslationALV.cs
+++ Liq_UI/Translation/TranslationALV.cs
@@ -159,6 +159,160 @@ namespace Liq_UI.Translation
             //*     I_INTERFACE_CHECK                 = ' '
             segmentALVCALL.CodeLines.Add("*     I_INTERFACE_CHECK                 = ' '");
 
+            //*     I_BYPASSING_BUFFER                = ' '
+            segmentALVCALL.CodeLines.Add("*     I_BYPASSING_BUFFER                = ' '");
+
+            //*     I_BUFFER_ACTIVE                   = ' '
+            segmentALVCALL.CodeLines.Add("*     I_BUFFER_ACTIVE                   = ' '");
+
+            //      I_CALLBACK_PROGRAM                = SY-CPROG
+            segmentALVCALL.CodeLines.Add("\t\t\tI_CALLBACK_PROGRAM                = SY-CPROG");
+
+            //*     I_CALLBACK_PF_STATUS_SET          = ' '
+            segmentALVCALL.CodeLines.Add("*     I_CALLBACK_PF_STATUS_SET          = ' '");
+
+            //*     I_CALLBACK_USER_COMMAND           = ' '
+            segmentALVCALL.CodeLines.Add("*     I_CALLBACK_USER_COMMAND           = ' '");
+
+            //*     I_CALLBACK_TOP_OF_PAGE            = ' '
+            segmentALVCALL.CodeLines.Add("*     I_CALLBACK_TOP_OF_PAGE            = ' '");
+
+            //*     I_CALLBACK_HTML_TOP_OF_PAGE       = ' '
+            segmentALVCALL.CodeLines.Add("*     I_CALLBACK_HTML_TOP_OF_PAGE       = ' '");
+
+            //*     I_CALLBACK_HTML_END_OF_LIST       = ' '
+            segmentALVCALL.CodeLines.Add("*     I_CALLBACK_HTML_END_OF_LIST       = ' '");
+
+            //*     I_STRUCTURE_NAME                  =
+            segmentALVCALL.CodeLines.Add("*     I_STRUCTURE_NAME                  =");
+
+            //*     I_BACKGROUND_ID                   = ' '
+            segmentALVCALL.CodeLines.Add("*     I_BACKGROUND_ID                   = ' '");
+
+            //*     I_GRID_TITLE                      =
+            segmentALVCALL.CodeLines.Add("*     I_GRID_TITLE  ");
+
+            //*     I_GRID_SETTINGS                   =
+            segmentALVCALL.CodeLines.Add("*     I_GRID_SETTINGS                   =");
+
+            //      IS_LAYOUT                         = IS_LAYOUT
+            segmentALVCALL.CodeLines.Add("\t\t\tIS_LAYOUT                         = IS_LAYOUT");
+
+            //      IT_FIELDCAT                       = IT_FIELDCAT[]
+            segmentALVCALL.CodeLines.Add("\t\t\tIT_FIELDCAT                       = IT_FIELDCAT[]");
+
+            //*     IT_EXCLUDING                      =
+            segmentALVCALL.CodeLines.Add("*     IT_EXCLUDING                      =");
+
+            //*     IT_SPECIAL_GROUPS                 =
+            segmentALVCALL.CodeLines.Add("*     IT_SPECIAL_GROUPS                 =");
+
+            //*     IT_SORT                           =
+            segmentALVCALL.CodeLines.Add("*     IT_SORT                           =");
+
+            //*     IT_FILTER                         =
+            segmentALVCALL.CodeLines.Add("*     IT_FILTER                         =");
+
+            //*     IS_SEL_HIDE                       =
+            segmentALVCALL.CodeLines.Add("*     IS_SEL_HIDE                       =");
+
+            //*     I_DEFAULT                         = 'X'
+            segmentALVCALL.CodeLines.Add("*     I_DEFAULT                         = 'X'");
+
+            //      I_SAVE                            = 'A'
+            segmentALVCALL.CodeLines.Add("      I_SAVE                            = 'A'");
+
+            //*     IS_VARIANT                        =
+            segmentALVCALL.CodeLines.Add("*     IS_VARIANT                        =");
+
+            //*     IT_EVENTS                         =
+            segmentALVCALL.CodeLines.Add("*     IT_EVENTS                         =");
+
+            //*     IT_EVENT_EXIT                     =
+            segmentALVCALL.CodeLines.Add("*     IT_EVENT_EXIT                     =");
+
+            //*     IS_PRINT                          =
+            segmentALVCALL.CodeLines.Add("*     IS_PRINT                          =");
+
+            //*     IS_REPREP_ID                      =
+            segmentALVCALL.CodeLines.Add("*     IS_REPREP_ID                      =");
+
+            //*     I_SCREEN_START_COLUMN             = 0
+            segmentALVCALL.CodeLines.Add("*     I_SCREEN_START_COLUMN             = 0");
+
+            //*     I_SCREEN_START_LINE               = 0
+            segmentALVCALL.CodeLines.Add("*     I_SCREEN_START_LINE               = 0");
+
+            //*     I_SCREEN_END_COLUMN               = 0
+            segmentALVCALL.CodeLines.Add("*     I_SCREEN_END_COLUMN               = 0");
+
+            //*     I_SCREEN_END_LINE                 = 0
+            segmentALVCALL.CodeLines.Add("*     I_SCREEN_END_LINE                 = 0");
+
+            //*     I_HTML_HEIGHT_TOP                 = 0
+            segmentALVCALL.CodeLines.Add("*     I_HTML_HEIGHT_TOP                 = 0");
+
+            //*     I_HTML_HEIGHT_END                 = 0
+            segmentALVCALL.CodeLines.Add("*     I_HTML_HEIGHT_END                 = 0");
+
+            //*     IT_ALV_GRAPHICS                   =
+            segmentALVCALL.CodeLines.Add("*     IT_ALV_GRAPHICS                   =");
+
+            //*     IT_HYPERLINK                      =
+            segmentALVCALL.CodeLines.Add("*     IT_HYPERLINK                      =");
+
+            //*     IT_ADD_FIELDCAT                   =
+            segmentALVCALL.CodeLines.Add("*     IT_ADD_FIELDCAT                   =");
+
+            //*     IT_EXCEPT_QINFO                   =
+            segmentALVCALL.CodeLines.Add("*     IT_EXCEPT_QINFO                   =");
+
+            //*     IR_SALV_FULLSCREEN_ADAPTER        =
+            segmentALVCALL.CodeLines.Add("*     IR_SALV_FULLSCREEN_ADAPTER        =");
+
+            //*   IMPORTING
+            segmentALVCALL.CodeLines.Add("*   IMPORTING");
+
+            //*     E_EXIT_CAUSED_BY_CALLER           =
+            segmentALVCALL.CodeLines.Add("*     E_EXIT_CAUSED_BY_CALLER           =");
+
+            //*     ES_EXIT_CAUSED_BY_USER            =
+            segmentALVCALL.CodeLines.Add("*     ES_EXIT_CAUSED_BY_USER            =");
+
+            //    TABLES
+            segmentALVCALL.CodeLines.Add("\t\tTABLES");
+
+            //      T_OUTTAB                          = T_ALV[]
+            segmentALVCALL.CodeLines.Add("\t\t\tT_OUTTAB                          = " 
+                + analysisResult.SplitterFormImpl.ALVTable.TableName + "[]");
+
+            //    EXCEPTIONS
+            segmentALVCALL.CodeLines.Add("\t\tEXCEPTIONS");
+
+            //      PROGRAM_ERROR                     = 1
+            segmentALVCALL.CodeLines.Add("\t\t\tPROGRAM_ERROR                     = 1");
+
+            //      OTHERS                            = 2
+            segmentALVCALL.CodeLines.Add("\t\t\tOTHERS                            = 2");
+
+            //            .
+            segmentALVCALL.CodeLines.Add("\t\t\t\t.");
+
+            //  IF SY-SUBRC <> 0.
+            segmentALVCALL.CodeLines.Add("\tIF SY-SUBRC <> 0.");
+
+            //* MESSAGE ID SY-MSGID TYPE SY-MSGTY NUMBER SY-MSGNO
+            segmentALVCALL.CodeLines.Add("* MESSAGE ID SY-MSGID TYPE SY-MSGTY NUMBER SY-MSGNO");
+
+            //*         WITH SY-MSGV1 SY-MSGV2 SY-MSGV3 SY-MSGV4.
+            segmentALVCALL.CodeLines.Add("*         WITH SY-MSGV1 SY-MSGV2 SY-MSGV3 SY-MSGV4.");
+
+            //  ENDIF.
+            segmentALVCALL.CodeLines.Add("\tENDIF.");
+
+            //ENDFORM
+            segmentALVCALL.CodeLines.Add("ENDFORM                    \" " + analysisResult.ALVCallImpl.FormName);
+
             //Append ALV CALL Segment
             segments.Add(segmentALVCALL);
 
