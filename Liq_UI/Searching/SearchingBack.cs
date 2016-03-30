490bb886ce487215c39733fff65e9c83ad591313
Boris Town <tbziy@hotmail.com>
2016-03-30 20:14:06 +0800

    Back Source Ralation to Filter

--- Liq_UI/Searching/SearchingBack.cs
+++ Liq_UI/Searching/SearchingBack.cs
@@ -7,11 +7,21 @@ namespace Liq_UI.Searching
 {
     internal class SearchingBack
     {
+        /// <summary>
+        /// Back Source Ralation to Filter
+        /// </summary>
+        /// <param name="sourceData">Source Data</param>
+        /// <param name="filterData">Filter Data</param>
         internal static void SourceBackToFilter(SourceData sourceData, FilterData filterData)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Back Source Ralation to Target
+        /// </summary>
+        /// <param name="sourceData">Source Data</param>
+        /// <param name="targetData">Target Data</param>
         internal static void SourceBackToTarget(SourceData sourceData, TargetData targetData)
         {
             throw new NotImplementedException();
