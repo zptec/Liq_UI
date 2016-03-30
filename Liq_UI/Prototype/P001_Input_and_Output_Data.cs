0619d591fefa04f591943eabd686869b8e7a599d
Boris Town <tbziy@hotmail.com>
2016-03-30 21:48:14 +0800

    P001_Input_and_Output_Data

---
+++ Liq_UI/Prototype/P001_Input_and_Output_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liq_UI.Prototype
+{
+    class P001_Input_and_Output_Data
+    {
+    }
+}
