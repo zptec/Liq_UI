2b7280304c107688763b80c98d72ddc4c15e97eb
Boris Town <tbziy@hotmail.com>
2016-03-30 22:46:52 +0800

    §1.1.3 Target Data

--- Liq_UI/Prototype/P001_Input_and_Output_Sample.cs
+++ Liq_UI/Prototype/P001_Input_and_Output_Sample.cs
@@ -11,13 +11,50 @@ namespace Liq_UI.Prototype
     /// </summary>
     class P001_Input_and_Output_Sample
     {
-        //§1 First Sample
+        /// §1 First Sample
 
-        //§1.1 Input
+        /// §1.1 Input
 
-        //§1.1.1 Source Data
+        /// §1.1.1 Source Data
 
+        /// <summary>
+        /// §1.1.1.1 VBAK Table Data
+        /// </summary>
+        string[,] VBAK = new string[,]
+        {
+            { "VBELN",          "KUNNR"     },
+            { "1200000249",     "LC00001"   },
+            { "1200000315",     "CWC01"     },
+            { "1300000096",     "LC00001"   }
+        };
 
-        string[,] VBAK = new string[3, 2];
+        /// <summary>
+        /// §1.1.1.2 KNA1 Table Data
+        /// </summary>
+        string[,] KNA1 = new string[,]
+        {
+            { "KUNNR",          "LAND1",        "NAME1"                                 },
+            { "CUST1400",       "TH",           "RTC (INTERNA"                          },
+            { "CUST1500",       "HK",           "CUST1500 CWMJ"                         },
+            { "CWC01",          "JP",           "CITIZEN WATCH CO., LTD."               },
+            { "K836",           "JP",           "KUMAGAI CO., LTD."                     },
+            { "LC00001",        "HK",           "KANKYOO KANTOKU (HONGKONG) CO.,LTD."   }
+        };
+
+        /// <summary>
+        /// §1.1.2 Filter Data
+        /// </summary>
+        string[,] FilterData = new string[,]
+        {
+            { "Customer",       "LC00001"       }
+        };
+
+        /// §1.1.3 Target Data
+        string[,] TargetData = new string[,]
+        {
+            { "Customer",       "Customer Name",                            "SO" },
+            { "LC00001",        "KANKYOO KANTOKU (HONGKONG) CO.,LTD.",      "1200000249" },
+            { "LC00001",        "KANKYOO KANTOKU (HONGKONG) CO.,LTD.",      "1300000096" }
+        };
     }
 }
